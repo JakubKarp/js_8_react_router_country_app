185f3e0859f35eabff18329360bb0e1dcefcfb72
JakubKarp <jakub.karp@innespacery.pl>
2018-01-18 09:25:09 +0100

    Dev tools

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/index.js';
 import { getCountries } from './actions/actions-countries';
-import DevTools from './DevTools'; // <DevTools /> 
+import DevTools from './DevTools';
 import { Router, hashHistory } from 'react-router';
 import routes from './routes';
 import './country.css';
 
 
 render(
-    <Provider store={store}>
-        <Router history={hashHistory} routes={routes} />
-    </Provider>,
+    <div>
+        <Provider store={store}>
+            <Router history={hashHistory} routes={routes} />
+        </Provider>
+        <DevTools />
+    </div> ,
 document.getElementById('root')
 );
 
